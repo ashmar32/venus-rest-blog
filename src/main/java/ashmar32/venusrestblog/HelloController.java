22d6b99dfa763672801c15a31af48e6d6fbfc1f5
Ashley Martinez <ashley.martinez32@gmail.com>
2022-09-06 15:54:11 -0500

    added path variable

--- src/main/java/ashmar32/venusrestblog/HelloController.java
+++ src/main/java/ashmar32/venusrestblog/HelloController.java
@@ -2,14 +2,15 @@ package ashmar32.venusrestblog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/hello/{name}")
     @ResponseBody
-    public String hello() {
-        return "Hello from Spring!";
+    public String hello(@PathVariable String name) {
+        return "Hello, " + name + "!";
     }
 }
