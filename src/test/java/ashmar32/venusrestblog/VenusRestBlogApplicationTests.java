e8a0ae6bdb002ff2da706de12a81eb0ebdbb681e
Ashley Martinez <ashley.martinez32@gmail.com>
2022-09-06 15:05:11 -0500

    first commit

---
+++ src/test/java/ashmar32/venusrestblog/VenusRestBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ashmar32.venusrestblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenusRestBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
